330bad23937e473e9aaeb32ce18f991281c14594
Joris Bertier <joris.bertier@gmail.com>
2025-04-11 15:07:05 -0500

    import module

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const { chain } = require('mathjs')
+
+
 const fs = require("fs");
-const data = fs.writeFile("fichier.txt", "utf8", (err, data) => {
-    if(err) {
-        console.log('Erreur')
-        return
-    }
-    console.log(data)
-});
-console.log(data);
+// const data = fs.writeFile("fichier.txt", "utf8", (err, data) => {
+//     if(err) {
+//         console.log('Erreur')
+//         return
+//     }
+//     console.log(data)
+// });
+console.log( chain(5).add(5).multiply(2));

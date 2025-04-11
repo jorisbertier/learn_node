ec411628ad299df84f0f70506cb4a5a8b451e5c6
Joris Bertier <joris.bertier@gmail.com>
2025-04-11 16:02:32 -0500

    manipuler json

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const argv = require('minimist')(process.argv.slice(2))
+const { readFileSync, writeFileSync, writeFile} = require('fs')
 
-console.log(argv.n)
-console.log(argv.a)
+// const { name, age, legal } = JSON.parse(readFileSync('./data.json'))
+
+// console.log(name)
+
+// const myData = {
+//     name:'Justine',
+//     age: 27,
+//     legal: true
+// }
+
+// const objectToJson = JSON.stringify(myData)
+
+// console.log(objectToJson)
+
+// writeFileSync('./myData.json', objectToJson)
+
+const myData = JSON.parse(readFileSync('./data.json'))
+
+myData.name = 'Pierre'
+
+const objectToJson = JSON.stringify(myData)
+writeFileSync('./myData.json', objectToJson)

11b1da6cbb933c2d5dd115e699aec81e0c3b29a5
Joris Bertier <joris.bertier@gmail.com>
2025-04-15 11:31:07 -0500

    handlebars

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path')
+const path = require('path');
+const { engine} = require("express-handlebars")
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, "views"))
 
 
-app.use(express.static(path.join(__dirname, "views")))
 app.use(express.static(path.join(__dirname, "public")))
-// app.get('/', (req, res) => {
-//     res.send('Hellow orld')
-// })
 
-app.get('/about', (req, res) => {
-    res.send('About')
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home'
+    })
 })
 
 app.listen(port, ()=> {
-    console.log('App listening on port ', port)
+    console.log('Listening port ', port)
 })

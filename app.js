2cfd7b3e093492a9fcc4497a7fd21038f47821c4
Joris Bertier <joris.bertier@gmail.com>
2025-04-15 11:07:09 -0500

    express static & listening port

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path')
 
 
-app.get('/', (req, res) => {
-    res.send('Hellow orld')
-})
+app.use(express.static(path.join(__dirname, "views")))
+app.use(express.static(path.join(__dirname, "public")))
+// app.get('/', (req, res) => {
+//     res.send('Hellow orld')
+// })
 
 app.get('/about', (req, res) => {
     res.send('About')
